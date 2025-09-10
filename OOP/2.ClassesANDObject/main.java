306e3c5746ad3015087afcef6ecc85cf241edb47
Souvick Kumar Halder <reach.souvick@gmail.com>
2025-09-10 23:54:23 +0530

    oop code added

--- OOP/2.ClassesANDObject/main.java
+++ OOP/2.ClassesANDObject/main.java
@@ -1,3 +1,22 @@
-public class main {
+class Person{
+    String Name;
+    int Age;
     
+    void input(String n, int a){
+        Name=n;
+        Age=a;
+    }
+    void output(){
+        System.out.println("Name: "+ Name);
+        System.out.println("Age: " + Age);
+
+    }
+}
+public class main {
+   public static void main(String args[]){
+    Person person1=new Person();
+    person1.input("Souvick", 20);
+    person1.output();
+   }
+
 }
